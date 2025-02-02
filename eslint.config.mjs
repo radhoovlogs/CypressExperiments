cbd90e87db745d3143e71a84d4ce1af0e14bc7fd
Radu.Popescu <radu.popescu@halfords.co.uk>
2025-02-02 11:19:25 +0200

    Update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  pluginCypress.configs.globals,
 ];
